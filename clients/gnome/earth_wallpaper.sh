d6858c85bfa9aa24403f8b90facd71d3431711dc
Lars Fischer <lars.fischer@rwth-aachen.de>
2021-01-27 21:15:52 +0100

    save wallpaper to config dir

--- clients/gnome/earth_wallpaper.sh
+++ clients/gnome/earth_wallpaper.sh
@@ -4,7 +4,7 @@ source ${HOME}/.config/earth_wallpaper/earth_wallpaper.config
 
 while [ 1 ]
 do
-    temp_file=$(mktemp)
+    temp_file=${HOME}/.config/earth_wallpaper/wallpaper.png
     wget -qO $temp_file ${DOMAIN}/wallpaper.png
     /usr/bin/gsettings set org.gnome.desktop.background picture-uri $temp_file
     sleep 600
