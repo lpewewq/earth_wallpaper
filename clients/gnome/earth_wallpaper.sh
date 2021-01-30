7793ae2c832e2defe8c407dc72a3bd2ae66352eb
Lars Fischer <lars.fischer@rwth-aachen.de>
2021-01-30 01:03:32 +0100

    modified client

--- clients/gnome/earth_wallpaper.sh
+++ clients/gnome/earth_wallpaper.sh
@@ -5,7 +5,7 @@ source ${HOME}/.config/earth_wallpaper/earth_wallpaper.config
 while [ 1 ]
 do
     temp_file=${HOME}/.config/earth_wallpaper/wallpaper.png
-    wget -qO $temp_file ${DOMAIN}/wallpaper.png
-    /usr/bin/gsettings set org.gnome.desktop.background picture-uri $temp_file
+    wget -qO $temp_file ${DOMAIN}/?resolution=${RESOLUTION}
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///${temp_file}
     sleep 600
 done
